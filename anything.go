e937e39addb10ea3eb648888f3a01932a55eb1b4
iusuf <iusuf@yahoo.com>
2020-01-03 08:31:01 -0800

    adds favs quotes

---
+++ anything.go
@@ -0,0 +1,23 @@
+package testRepo
+
+func Favs( )[]string {
+	return []string{
+		"Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.",
+
+"Stop acting so small. You are the universe in ecstatic motion.",
+
+"What you seek is seeking you.",
+
+"Don’t grieve. Anything you lose comes round in another form.",
+
+"Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.",
+
+"Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.",
+
+"Don’t be satisfied with stories, how things have gone with others. Unfold your own myth.",
+
+"Everything that is made beautiful and fair and lovely is made for the eye of one who sees.",
+
+"Why should I be unhappy? Every parcel of my being is in full bloom."
+	}
+}
